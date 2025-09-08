699cacd8122dd8c83e602bd38c1d5b1560401cc5
YuukiNobori-amulapo <oolaafee@gmail.com>
2025-09-08 09:55:51 +0900

    Qiita-Autologin

---
+++ qiita_AutoLogin.py
@@ -0,0 +1,26 @@
+# seleniumの基本的な更新
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+#Chromeドライバマネジャーの更新
+service = Service(ChromeDriverManager().install)
+driver = webdriver.Chrome(service=service)
+
+#新たにChromeのWEBサイトにアクセスする
+driver =webdriver.Chrome()
+driver.get("https://qiita.com/login?callback_action=login_or_signup&redirect_to=%2F&realm=qiita")
+
+#ユーザー名とパスワードの欄を探す
+identity = driver.find_element(By.ID, "identity")
+password = driver.find_element(By.ID, "password")
+
+#ユーザー名、パスワードを入力
+identity.send_keys("ramunedayo")
+password.send_keys("Seruka373")
+
+#ログインする 
+login_button = driver.find_element(By.XPATH, "//input[@type='submit']")
+login_button.click()
